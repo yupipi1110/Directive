57ac9403e2dec0a27a2123f49cea9e53adaea090
yupipi1110 <126690147+yupipi1110@users.noreply.github.com>
2023-05-24 22:20:29 +0900

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
+  
+  watchNgIf = true;
 
-  isLoggedIn=true;
+  items = ['Item 1', 'Item 2', 'Item 3'];
 
-  };
-
+  value = 'C';
+}
