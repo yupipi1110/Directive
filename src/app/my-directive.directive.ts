fe3257b545c57cb10b064b3cb0f7c349d5a9f45f
yupipi1110 <126690147+yupipi1110@users.noreply.github.com>
2023-05-25 13:37:33 +0900

    1 file modified

--- src/app/my-directive.directive.ts
+++ src/app/my-directive.directive.ts
@@ -4,13 +4,18 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
   selector: '[myDirective]',
 })
 export class MyDirectiveDirective {
+  //ElementRefを注入することでカスタムディレクティブが適応された要素を使用可能になる。
+  //DOM要素への参照が取得できるということ。
   constructor(private elementRef: ElementRef) {}
 
+  //マウスを乗せた際に実行されるメソッド
+  //@HostListenerデコレータを使用することで指定したイベントが発生した際にメソッドが実行される。
   @HostListener('mouseenter')
   onMouseEnter() {
     this.elementRef.nativeElement.style.backgroundColor = 'yellow';
   }
 
+  //マウスを離した際に実行されるメソッド
   @HostListener('mouseleave')
   onMouseLeave() {
     this.elementRef.nativeElement.style.backgroundColor = '';
