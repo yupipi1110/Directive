79440560cac7f4ba41c346144b27a9769e8fdf96
yupipi1110 <126690147+yupipi1110@users.noreply.github.com>
2023-05-25 09:39:58 +0900

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
+  //ここから構造ディレクティブ
   watchNgIf = true;
 
   watchNgFors = ['Item 1', 'Item 2', 'Item 3'];
@@ -20,7 +20,11 @@ export class AppComponent {
   toggleButtonVisibility() {
     this.isButtonVisible = !this.isButtonVisible;
 
-  //ここまでが構造ディレクティブ、以降は属性ディレクティブ
+    //ここまでが構造ディレクティブ、以降は属性ディレクティブ
 
+    let name = [
+      { name: '山田', description: '私は山田です。' },
+      { name: '田中', description: '私は田中です。' },
+    ];
   }
 }
